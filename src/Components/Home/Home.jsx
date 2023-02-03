b39150a5a64db6830a511404b81817d2a6b43444
Abdelwahab Mohamed <abdelwahab_en30895002@f-eng.tanta.edu.eg>
2023-02-03 15:10:53 +0200

    view

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,7 +12,31 @@ export default function Home() {
           <h2>Filters</h2>
         </div>
         {/* Jobs */}
-        <div className="col-md-8"></div>
+        <div className="col-md-8">
+        {
+          jobs.map((job,index) => (
+            <div className="col-md-8" key={index}>
+            <div className="card mb-3">
+              <div className="row no-gutters">
+                <div className="col-md-4">
+                  <img src={job.job_img_url} className="card-img" alt="Job Image" />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{job.job_name}</h5>
+                    <p className="card-text">{job.job_description}</p>
+                    <p className="card-text">
+                      <small className="text-muted">{job.publisher}</small>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  ))
+        }
+        </div>
+
       </div>
     </>
   );
