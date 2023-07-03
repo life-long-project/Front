90b30042f19c749f91752b3d9ce14b3840d880ca
Mostafa-Hussiny <113726825+Mostafa-Hussiny@users.noreply.github.com>
2023-07-03 23:44:02 +0300

    Firebase deployment

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6KJvZxucXPdv1yEtG2ijxN3zqeigq4DI",
+  authDomain: "snapjobs-a7d7e.firebaseapp.com",
+  projectId: "snapjobs-a7d7e",
+  storageBucket: "snapjobs-a7d7e.appspot.com",
+  messagingSenderId: "994748338625",
+  appId: "1:994748338625:web:9eec857f91e8f13adb1f40",
+};
+
+//init firebase
+initializeApp(firebaseConfig);
