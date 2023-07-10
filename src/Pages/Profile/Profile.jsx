e8ec84b12dc163ee35a406757884fab4ed1e438e
Abdelwahab Mohamed <abdelwahabmo98@gmail.com>
2023-07-10 11:06:49 +0300

    new card added and handle profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,84 +1,179 @@
 import React from "react";
 import "./Profile.css";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Rating from "react-rating";
 import { BsStar, BsStarFill } from "react-icons/bs";
 import { AiFillDollarCircle } from "react-icons/ai";
 import { RiShieldStarLine } from "react-icons/ri";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
+import { MdEmail } from "react-icons/md";
+import { FaUserAlt } from "react-icons/fa";
+import { useAxiosGet } from "../../Hooks/useAxiosGet";
+import moment from "moment";
+import Loading from "../LoadingPage/LoadingPage";
+import JobList from "../Jobs/JobList/JobList";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 export default function Profile() {
+  const { id } = useParams();
+  const { data, isPending, error } = useAxiosGet(
+    `https://back-ph2h.onrender.com/user/profile/${id}`
+  );
+  console.log(data);
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="profile mt-5">
-              <div className="row">
-                <div className="col-md-4">
-                  <div className="profile-sidebar">
-                    <div className="profile-img">
-                      <img
-                        src="https://bootdey.com/img/Content/avatar/avatar7.png"
-                        alt=""
-                      />
+      {isPending && <Loading />}
+      {data && (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="profile mt-5">
+                <div className="row">
+                  <div className="col-md-4">
+                    <div className="profile-sidebar">
+                      <div className="profile-img">
+                        <img src={data.user.profile_url} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="profile-user-info">
+                      <h2>{data.user.full_name}</h2>
+                      <p>{data.user.past_experience}</p>
+                      <div className="rating-Profile">
+                        <Rating
+                          className="ratingProfile"
+                          emptySymbol={<BsStarFill className="emptySymbol" />}
+                          fullSymbol={<BsStarFill className="fullSymbol" />}
+                          fractions={2}
+                          readonly
+                          initialRating={data.user.rating}
+                        />{" "}
+                        <span className="ms-2">
+                          {data.user.rating} ({data.rates.length} reviews)
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="profile-user-personal-details">
+                      <ul className="list-group">
+                        <li className="list-group-item">
+                          <strong>Total Earning : </strong>
+                          <AiFillDollarCircle className="d-inline ai-dollar-icon" />{" "}
+                          <span className="text-muted">250 LE</span>
+                        </li>
+                        <li className="list-group-item">
+                          <strong>Jobs Completed : </strong>
+                          <span className="text-muted">
+                            <span>{data.accepted_jobs.length}</span> Jobs
+                          </span>
+                        </li>
+                        <li className="list-group-item">
+                          <strong>Jobs Published : </strong>
+                          <span className="text-muted">
+                            <span>{data.user_jobs.length}</span> Jobs
+                          </span>
+                        </li>
+                        <li className="list-group-item">
+                          <strong>Location : </strong>
+                          <span className="text-muted">
+                            EGYPT , {data.user.city}
+                          </span>
+                        </li>
+                        <li className="list-group-item">
+                          <strong className="text-muted me-2 pb-2">
+                            <RiShieldStarLine className="d-inline" />{" "}
+                          </strong>
+                          <span className="text-muted">
+                            Joined {moment().format("MMM Do YY")}
+                          </span>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </div>
-                <div className="col-md-4">
-                  <div className="profile-user-info">
-                    <h2>John Doe</h2>
-                    <p>Web Designer / UI.</p>
-                    <div className="rating-Profile">
-                      <Rating
-                        className="ratingProfile"
-                        emptySymbol={<BsStarFill className="emptySymbol" />}
-                        fullSymbol={<BsStarFill className="fullSymbol" />}
-                        fractions={2}
-                        // readonly
-                        // initialRating={job[0].user.total_rating}
-                      />{" "}
-                      <span className="ms-2">0.0 (0 reviews)</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-8">
+              <div className="profile-bottom-section mt-3">
+                <Tabs>
+                  <TabList>
+                    <Tab>Reviews</Tab>
+                    <Tab>Applied Jobs</Tab>
+                    <Tab>Published Jobs</Tab>
+                  </TabList>
+
+                  <TabPanels>
+                    <TabPanel>
+                      <p>one!</p>
+                    </TabPanel>
+                    <TabPanel>
+                      <JobList jobs={data.accepted_jobs} />
+                    </TabPanel>
+                    <TabPanel>
+                      <div className="row">
+                        <JobList jobs={data.user_jobs} />
+                      </div>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="profile-bottom-section mt-3">
+                <div className="bottom-section-rhs-title">
+                  <h4>Verifications</h4>
+                </div>
+                <div className="bottom-section-rhs-content mt-4">
+                  <div className="row mb-2">
+                    <div className="col-1">
+                      <div className="verfiryIcon">
+                        <MdEmail className="d-inline" />
+                      </div>
+                    </div>
+                    <div className="col-5">
+                      <p>Email</p>
+                    </div>
+                    <div className="col-6 d-flex justify-content-end">
+                      <p className="text-muted">Verified</p>
                     </div>
                   </div>
-                </div>
-                <div className="col-md-4">
-                  <div className="profile-user-personal-details">
-                    <ul className="list-group">
-                      <li className="list-group-item">
-                        <strong>Total Earning : </strong>
-                        <AiFillDollarCircle className="d-inline ai-dollar-icon" />{" "}
-                        <span className="text-muted">250 LE</span>
-                      </li>
-                      <li className="list-group-item">
-                        <strong>Jobs Completed : </strong>
-                        <span className="text-muted">
-                          <span>11</span> Jobs
-                        </span>
-                      </li>
-                      <li className="list-group-item">
-                        <strong>Jobs Published : </strong>
-                        <span className="text-muted">
-                          <span>21</span> Jobs
-                        </span>
-                      </li>
-                      <li className="list-group-item">
-                        <strong>Location : </strong>
-                        <span className="text-muted">EGYPT , Gharbia</span>
-                      </li>
-                      <li className="list-group-item">
-                        <strong className="text-muted me-2 pb-2"><RiShieldStarLine className="d-inline"/> </strong>
-                        <span className="text-muted">
-                          Joined August 18, 2017
-                        </span>
-                      </li>
-                    </ul>
+                  <div className="row mb-2">
+                    <div className="col-1">
+                      <div className="verfiryIcon">
+                        <BsFillTelephoneFill className="d-inline" />
+                      </div>
+                    </div>
+                    <div className="col-5">
+                      <p>Phone Number</p>
+                    </div>
+                    <div className="col-6 d-flex justify-content-end">
+                      <p className="text-muted">Verified</p>
+                    </div>
+                  </div>
+                  <div className="row mb-2">
+                    <div className="col-1">
+                      <div className="verfiryIcon">
+                        <FaUserAlt className="d-inline" />
+                      </div>
+                    </div>
+                    <div className="col-5">
+                      <p>Identity Verified</p>
+                    </div>
+                    <div className="col-6 d-flex justify-content-end">
+                      <p className="text-muted">Verified</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
